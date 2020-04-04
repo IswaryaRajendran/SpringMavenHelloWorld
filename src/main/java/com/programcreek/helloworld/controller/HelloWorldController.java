05f7b7587b13b670c6ffd7a312e1cb228599cf8c
IswaryaRajendran <53403770+IswaryaRajendran@users.noreply.github.com>
2020-04-05 02:51:09 +0530

    Checking
    Checking

--- src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -14,7 +14,7 @@ public class HelloWorldController {
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in Controller");
+		System.out.println("in controller");
 
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
